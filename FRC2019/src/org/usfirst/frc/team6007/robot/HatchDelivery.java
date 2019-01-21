167302a74f72db697e6c3e52978a72cae49186cf
Jordan <46876926+JordankHackerman@users.noreply.github.com>
2019-01-22 09:37:04 +1100

    Update HatchDelivery.java

--- FRC2019/src/org/usfirst/frc/team6007/robot/HatchDelivery.java
+++ FRC2019/src/org/usfirst/frc/team6007/robot/HatchDelivery.java
@@ -1,15 +1,23 @@
 /************************************************************
-* Robotics Hatch Delivery by Jordan Thorne date 21/01/19	
+* Robotics Hatch Delivery by Jordan Thorne date 22/01/19	
 ************************************************************/
 package org.usfirst.frc.team6007.robot;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive; 
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.command.Subsystem;
 
 
 public class HatchDelivery{
   
+	DifferentialDrive HatchBase;
 	private static RobotIO hatchtLifterPotentiometer;
   	private static ReadSwitch homePosition;
   	private static ReadSwitch floorPosition;
-
+	private Spark leftHatchMotor;
+	private Spark rightHatchMotor;
+	
 	public HatchDelivery(){
     
 		leftHatchMotor = new Spark(RobotMap.PWM_PinOut.LEFT_HATCH_MOTOR_ID);
@@ -19,14 +27,14 @@ public class HatchDelivery{
 	
 	}
 	
-	//Picks up Disk
-	public void LiftUp(double Power){
+	//Picks up Disk or something
+	public void Up(double Power){
 		HatchBase.arcadeDrive(Power, 0);
 	}
 	
-	//Puts down Disk
-	public void PutDown(double DownPower){
-		HatchBase.arcadeDrive(DownPower, 0);
+	//Puts down Disk or something 
+	public void Down(double DownPower){
+		HatchBase.arcadeDrive(DownPower, 0);	
 	}
 	
 	
