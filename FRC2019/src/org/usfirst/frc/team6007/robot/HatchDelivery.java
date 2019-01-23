9dcec263ad086e6a650b03e904ef839a63e7aeea
Jordan <46876926+JordankHackerman@users.noreply.github.com>
2019-01-23 16:04:14 +1100

    Update HatchDelivery.java
    I think i forgot somethings.

--- FRC2019/src/org/usfirst/frc/team6007/robot/HatchDelivery.java
+++ FRC2019/src/org/usfirst/frc/team6007/robot/HatchDelivery.java
@@ -1,33 +1,63 @@
-/************************************************************
-* Robotics Hatch Delivery by Jordan Thorne date 21/01/19	
-************************************************************/
+/*******************************************************
+* Robotics Hatch Delivery by Jordan Thorne date 23/01/19	
+*******************************************************/
+/***********************************
+*Arm that retrives and delivers disk
+***********************************/
 package org.usfirst.frc.team6007.robot;
-
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive; 
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class HatchDelivery{
   
+	DifferentialDrive hatchBase;
 	private static RobotIO hatchtLifterPotentiometer;
-  	private static ReadSwitch homePosition;
-  	private static ReadSwitch floorPosition;
-
+	private RobotIO robotIO;
+	private VictorSP HatchMotor;
+	
 	public HatchDelivery(){
     
-		leftHatchMotor = new Spark(RobotMap.PWM_PinOut.LEFT_HATCH_MOTOR_ID);
-		rightHatchMotor = new Spark(RobotMap.PWM_PinOut.RIGHT_HATCH_MOTOR_ID);
-    
-		HatchBase = new DifferentialDrive(leftHatchMotor, rightHatchMotor);
-	
+		HatchMotor = new VictorSP(RobotMap.PWM_PinOut.RIGHT_HATCH_MOTOR_ID);
+   
+	}
+
+	//Puts disk in home position
+	public void hatchLifterToHomePosition(double Power){
+		while(!robotIO.hatchSwitchAtHome){
+			HatchMotor.set(Power, 0);
+			//while so it runs until its false
+		}
+		
 	}
 	
-	//Picks up Disk
-	public void LiftUp(double Power){
-		HatchBase.arcadeDrive(Power, 0);
+	//Puts arm in delivery position
+	public void deliveryPosition(double Power) {
+		HatchMotor.set (power, 0) ;
+		
+	if (HatchDelivery.getCurrentLiftDistance() <
 	}
+		
+		if (hatchDelivery.getCurrentLiftDistance() < target - 20 ) {
+			hatchDelivery.setLiftPower(0.7);
+		} else if (hatchDelivery.getCurrentLiftDistance() > target + 20) {
+		
+			hatchDelivery.setLiftPower(0.3);
+		} else {
+		
+			hatchDelivery.setLiftPower (RobotMap.HOLD_POWER);
+		}
 	
-	//Puts down Disk
-	public void PutDown(double DownPower){
-		HatchBase.arcadeDrive(DownPower, 0);
+	hatchDelivery.liftup(HatchDelivery.getLiftPower());*/
+}	
+				
+	//Retrieves the hatch from floor
+	public void retriveHatchFromFloor(double DownPower){
+		while (!robotIO.hatchSwitchAtLower){
+			HatchMotor.set(DownPower, 0);	
+		}
+		
 	}
-	
 	
   }
